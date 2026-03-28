7b1825c842c9f06f99f2826d1be4257cbaafa502
RamaTulasi2006 <botlaguntatulasi27@gmail.com>
2026-03-28 12:57:29 +0530

    Update dsp_utils.py

--- dsp_utils.py
+++ dsp_utils.py
@@ -1,75 +1,32 @@
 import numpy as np
-from scipy import signal
+from scipy.signal import butter, lfilter
 
-# Signal Generator
-def generate_signal(signal_type, freq, fs, duration):
+# Generate ECG Signal
+def generate_ecg(fs, duration):
+    t = np.linspace(0, duration, fs * duration)
+    ecg = np.sin(2 * np.pi * 1.7 * t) + 0.5 * np.sin(2 * np.pi * 2.1 * t)
+    return t, ecg
 
-    t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+# Add Noise
+def add_noise(signal, noise_level):
+    noise = noise_level * np.random.randn(len(signal))
+    noisy_signal = signal + noise
+    return noisy_signal, noise
 
-    if signal_type == "Sine":
-        x = np.sin(2 * np.pi * freq * t)
-
-    elif signal_type == "Square":
-        x = signal.square(2 * np.pi * freq * t)
-
-    elif signal_type == "Triangle":
-        x = signal.sawtooth(2 * np.pi * freq * t, 0.5)
-
-    elif signal_type == "Sawtooth":
-        x = signal.sawtooth(2 * np.pi * freq * t)
-
-    elif signal_type == "Chirp":
-        x = signal.chirp(t, f0=1, f1=freq, t1=duration)
-
-    elif signal_type == "White Noise":
-        x = np.random.normal(0, 1, len(t))
-
-    elif signal_type == "Impulse":
-        x = np.zeros(len(t))
-        x[0] = 1
-
-    elif signal_type == "Step":
-        x = np.ones(len(t))
-
-    else:
-        x = np.sin(2 * np.pi * freq * t)
-
-    return t, x
-
+# Butterworth Filter
+def butter_filter(signal, fs, cutoff, order):
+    b, a = butter(order, cutoff, fs=fs)
+    filtered_signal = lfilter(b, a, signal)
+    return filtered_signal
 
 # FFT
-def compute_fft(x, fs):
+def compute_fft(signal, fs):
+    fft_vals = np.fft.fft(signal)
+    freq = np.fft.fftfreq(len(signal), 1/fs)
+    return freq, np.abs(fft_vals)
 
-    X = np.fft.fft(x)
-    freqs = np.fft.fftfreq(len(X), 1/fs)
-
-    mask = freqs >= 0
-
-    return freqs[mask], np.abs(X[mask])
-
-
-# Low Pass Filter
-def apply_lowpass_filter(x, cutoff, fs):
-
-    b, a = signal.butter(4, cutoff/(fs/2), btype="low")
-
-    y = signal.lfilter(b, a, x)
-
-    return y
-
-
-# Convolution
-def apply_convolution(x):
-
-    h = np.ones(20) / 20
-    y = np.convolve(x, h, mode="same")
-
-    return y
-
-
-# Window Functions
-def apply_window(window_type, length):
-
-    window = signal.get_window(window_type, length)
-
-    return window
+# SNR Calculation
+def compute_snr(original, noise, filtered):
+    snr_before = np.mean(original**2) / np.mean(noise**2)
+    snr_after = np.mean(original**2) / np.mean((filtered - original)**2)
+    return snr_before, snr_after
